c6facb9cd4fb0344a50ab488884f0aa4e565f8a3
Sergio Azizi <sergio.azizi.14@gmail.com>
2021-09-15 23:26:34 +0100

    feat: add tweakPane package and debug method to Experience class

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,145 +1,128 @@
-import * as THREE from 'three'
-import Time from './Utils/Time.js'
-import Sizes from './Utils/Sizes.js'
-import Stats from './Utils/Stats.js'
+import * as THREE from "three";
+import { Pane } from "tweakpane";
+import Time from "./Utils/Time.js";
+import Sizes from "./Utils/Sizes.js";
+import Stats from "./Utils/Stats.js";
 
-import Resources from './Resources.js'
-import Renderer from './Renderer.js'
-import Camera from './Camera.js'
-import World from './World.js'
+import Resources from "./Resources.js";
+import Renderer from "./Renderer.js";
+import Camera from "./Camera.js";
+import World from "./World.js";
 
-import assets from './assets.js'
+import assets from "./assets.js";
 
-export default class Experience
-{
-    static instance
+export default class Experience {
+  static instance;
 
-    constructor(_options = {})
-    {
-        if(Experience.instance)
-        {
-            return Experience.instance
-        }
-        Experience.instance = this
+  constructor(_options = {}) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+    Experience.instance = this;
 
-        // Options
-        this.targetElement = _options.targetElement
+    // Options
+    this.targetElement = _options.targetElement;
 
-        if(!this.targetElement)
-        {
-            console.warn('Missing \'targetElement\' property')
-            return
-        }
-
-        this.time = new Time()
-        this.sizes = new Sizes()
-        this.setConfig()
-        this.setStats()
-        this.setScene()
-        this.setCamera()
-        this.setRenderer()
-        this.setResources()
-        this.setWorld()
-        
-        this.sizes.on('resize', () =>
-        {
-            this.resize()
-        })
-
-        this.update()
+    if (!this.targetElement) {
+      console.warn("Missing 'targetElement' property");
+      return;
     }
 
-    setConfig()
-    {
-        this.config = {}
-    
-        // Debug
-        this.config.debug = window.location.hash === '#debug'
+    this.time = new Time();
+    this.sizes = new Sizes();
+    this.setConfig();
+    this.setStats();
+    this.setDebug();
+    this.setScene();
+    this.setCamera();
+    this.setRenderer();
+    this.setResources();
+    this.setWorld();
 
-        // Pixel ratio
-        this.config.pixelRatio = Math.min(Math.max(window.devicePixelRatio, 1), 2)
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
 
-        // Width and height
-        const boundings = this.targetElement.getBoundingClientRect()
-        this.config.width = boundings.width
-        this.config.height = boundings.height || window.innerHeight
+    this.update();
+  }
+
+  setConfig() {
+    this.config = {};
+
+    // Debug
+    this.config.debug = window.location.hash === "#debug";
+
+    // Pixel ratio
+    this.config.pixelRatio = Math.min(Math.max(window.devicePixelRatio, 1), 2);
+
+    // Width and height
+    const boundings = this.targetElement.getBoundingClientRect();
+    this.config.width = boundings.width;
+    this.config.height = boundings.height || window.innerHeight;
+  }
+
+  setStats() {
+    if (this.config.debug) {
+      this.stats = new Stats(true);
     }
-
-    setStats()
-    {
-        if(this.config.debug)
-        {
-            this.stats = new Stats(true)
-        }
+  }
+  setDebug() {
+    if (this.config.debug) {
+      this.debug = new Pane();
+      this.debug.containerElem_.style.width = "320px";
     }
-    
-    setScene()
-    {
-        this.scene = new THREE.Scene()
-    }
+  }
 
-    setCamera()
-    {
-        this.camera = new Camera()
-    }
+  setScene() {
+    this.scene = new THREE.Scene();
+  }
 
-    setRenderer()
-    {
-        this.renderer = new Renderer({ rendererInstance: this.rendererInstance })
+  setCamera() {
+    this.camera = new Camera();
+  }
 
-        this.targetElement.appendChild(this.renderer.instance.domElement)
-    }
+  setRenderer() {
+    this.renderer = new Renderer({ rendererInstance: this.rendererInstance });
 
-    setResources()
-    {
-        this.resources = new Resources(assets)
-    }
+    this.targetElement.appendChild(this.renderer.instance.domElement);
+  }
 
-    setWorld()
-    {
-        this.world = new World()
-    }
+  setResources() {
+    this.resources = new Resources(assets);
+  }
 
-    update()
-    {
-        if(this.stats)
-            this.stats.update()
-        
-        this.camera.update()
+  setWorld() {
+    this.world = new World();
+  }
 
-        if(this.world)
-            this.world.update()
-        
-        if(this.renderer)
-            this.renderer.update()
+  update() {
+    if (this.stats) this.stats.update();
 
-        window.requestAnimationFrame(() =>
-        {
-            this.update()
-        })
-    }
+    this.camera.update();
 
-    resize()
-    {
-        // Config
-        const boundings = this.targetElement.getBoundingClientRect()
-        this.config.width = boundings.width
-        this.config.height = boundings.height
+    if (this.world) this.world.update();
 
-        this.config.pixelRatio = Math.min(Math.max(window.devicePixelRatio, 1), 2)
+    if (this.renderer) this.renderer.update();
 
-        if(this.camera)
-            this.camera.resize()
+    window.requestAnimationFrame(() => {
+      this.update();
+    });
+  }
 
-        if(this.renderer)
-            this.renderer.resize()
+  resize() {
+    // Config
+    const boundings = this.targetElement.getBoundingClientRect();
+    this.config.width = boundings.width;
+    this.config.height = boundings.height;
 
-        if(this.world)
-            this.world.resize()
-    }
+    this.config.pixelRatio = Math.min(Math.max(window.devicePixelRatio, 1), 2);
 
-    destroy()
-    {
-        
-    }
+    if (this.camera) this.camera.resize();
+
+    if (this.renderer) this.renderer.resize();
+
+    if (this.world) this.world.resize();
+  }
+
+  destroy() {}
 }

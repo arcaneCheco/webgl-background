ce7ef57ccdcacd17ab3f40052385a26ead3ff8a5
Sergio Azizi <sergio.azizi.14@gmail.com>
2021-09-15 23:22:07 +0100

    feat: switch to shader material

--- src/Experience/Gradient.js
+++ src/Experience/Gradient.js
@@ -1,5 +1,31 @@
+import * as THREE from "three";
+import Experience from "./Experience";
+import vertexShader from "./shaders/gradient/vertex.glsl";
+import fragmentShader from "./shaders/gradient/fragment.glsl";
+
 export default class Gradient {
   constructor() {
-    console.log("test");
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+
+    this.setGeometry();
+    this.setMaterial();
+    this.setMesh();
+  }
+
+  setGeometry() {
+    this.geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+  }
+
+  setMaterial() {
+    this.material = new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+    });
+  }
+
+  setMesh() {
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
   }
 }
